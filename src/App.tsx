7a01aeb2ff36036154cd7af7df477f341a461f76
lucarvalho7 <lucianocarvallho7@gmail.com>
2020-08-10 10:49:03 -0300

    front-end da aplicação

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+
+import Routes from './routtes';
+
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+export interface Teacher {
+  id:number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+};
+
+
+export interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const  TeacherItem: React.FC<TeacherItemProps> = ({ teacher})=> {
+  return(
+    <article className="teacher-item">
+            <header>
+              <img src={teacher.avatar} alt={teacher.name}  />
+              <div>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
+              </div>
+            </header>
+            <p>{teacher.bio}</p>
+            <footer>
+              <p>
+                Preço/Hora
+                <strong>{teacher.cost}</strong>
+              </p>
+              <button type="button">
+                <img src={whatsappIcon} alt="Whatsapp" />
+                Entrar em contato
+                </button>
+            </footer>
+          </article>
+  )
+}
+
+export default TeacherItem;
